da721eb200bbf4b4bcd31b42fd52b2c5a8e0cc95
Visnusah <240152@softwarica.edu.np>
2026-05-18 10:42:47 +0545

    feat: add server-side redirect functionality with action handler and example page

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -4,7 +4,7 @@ import { NextPage } from 'next'
 interface Props {}
 
 const Page: NextPage<Props> = ({}) => {
-  return <div>Hello Kamlesh</div>
+  return <div>Hello This is example Page</div>
 }
 
 export default Page
