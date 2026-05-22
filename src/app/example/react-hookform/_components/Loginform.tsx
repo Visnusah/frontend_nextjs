2585c5f71d08857336056a910a2ebc6b7539e2ca
Visnusah <240152@softwarica.edu.np>
2026-05-22 09:23:07 +0545

    feat: implement LoginForm component with form handling and submission alert

--- src/app/example/react-hookform/_components/Loginform.tsx
+++ src/app/example/react-hookform/_components/Loginform.tsx
@@ -1,6 +1,19 @@
-
-
-export default function LoginFrom() {
+"use client";
+import { useForm } from "react-hook-form";
+export default function LoginForm() {
+    const {
+        register, // to implement in input
+        handleSubmit, // to implement submission
+        formState: { errors, isSubmitting }
+    } = useForm({
+        defaultValues: {
+            email: "", // states default
+            password: ""
+        }
+    });
+    const onSubmit = (data: any) => {
+        alert("Submitted data: " + data.email + ", " + data.password);
+    }
     return (
         <div></div>
     );
