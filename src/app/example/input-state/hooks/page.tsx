9ce99464b9f04000b589d364ae27d2e71cc2878b
Visnusah <240152@softwarica.edu.np>
2026-05-22 08:41:46 +0545

    mess:

---
+++ src/app/example/input-state/hooks/page.tsx
@@ -0,0 +1,15 @@
+// convert this statemanagement into hooke
+// states for email, password
+// handleChange for both
+// handleSubmit to alert email and password
+// in input-state/hooks/use-form.tsx
+// in input-state/hooks/page.tsx use the useForm hook 
+// and create form for email and password
+
+'"use client'
+import { useState, useEffect } from "react";
+export default function Page() {
+    return (
+        <div></div>
+    );
+}
